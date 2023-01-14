11cbd1500bd06c6e532c55349c8ed762fbd73a97
Kshitiz Khanal <kshitizkhanal360@gmail.com>
2023-01-14 12:12:43 +0545

    Completed

--- src/api.py
+++ src/api.py
@@ -6,8 +6,21 @@ import matplotlib.pyplot as plt
 import datetime
 from pycoingecko import CoinGeckoAPI
 
-class bitcoin:
+class BitCoin:
     def __init__(self):
-        cg = self.CoinGeckoAPI()
+        self.data_to_return = {}
+    
+    def get_values(self):
+        cg = CoinGeckoAPI()
         bitcoin_data = cg.get_coin_market_chart_by_id(id='bitcoin', vs_currency='usd', days=30)
         bitcoin_price_data = bitcoin_data['prices']
+        data = pd.DataFrame(bitcoin_price_data, columns=['TimeStamp', 'Price'])
+        data['date'] = data['TimeStamp'].apply(lambda d: datetime.date.fromtimestamp(d/1000.0))
+        self.data_to_return = data.iloc[0:,1:]
+        return self.data_to_return
+        
+
+bit = BitCoin()
+
+data = bit.get_values()
+print(data)
