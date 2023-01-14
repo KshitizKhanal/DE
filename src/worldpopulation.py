ea7aaff77b657e8874d7317d9cdd63243cbcb667
Kshitiz Khanal <kshitizkhanal360@gmail.com>
2023-01-14 19:18:13 +0545

    10 most densely pop countries

--- src/worldpopulation.py
+++ src/worldpopulation.py
@@ -1,10 +1,34 @@
 import pandas as pd
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as BS
 import requests as r
 url = 'https://en.wikipedia.org/wiki/World_population'
 
-soup = r.get(url).text
-print(soup)
+data = r.get(url).text
+soup = BS(data,'html.parser')
+
+tables = soup.find_all('table')
+
+
+for i,table in enumerate(tables):
+    if ("10 most densely populated countries" in str(table)):
+        table_index = i
+        break
+
+# population_data = pd.DataFrame(columns=["Rank","Country","Density"])
+# for row in tables[table_index].tbody.find_all("tr"):
+#     col = row.find_all("td")
+#     if (col != []):
+#         rank = col[0].text
+#         country = col[1].text
+#         density = col[4].text.strip()
+#         population_data =  population_data.append({"Rank":rank,"Country":country,"Density":density},ignore_index=True)
+
+# print(population_data)
+
+pop_data = pd.read_html(str(tables[table_index]),flavor='bs4')
+print(pop_data)
+
+
 
 
 
